cc5ef83b7fb15f18d2f2f8f084e725d02e94f1cd
licker2689 <83544855+licker2689@users.noreply.github.com>
2023-02-04 12:25:53 +0900

    Initial Commit

---
+++ v1_12_2/src/main/java/com/darksoldier1404/dpm/events/DPMEvent.java
@@ -0,0 +1,171 @@
+package com.darksoldier1404.dpm.events;
+
+import com.darksoldier1404.dpm.Menu;
+import com.darksoldier1404.dpm.functions.DPMFunction;
+import com.darksoldier1404.dppc.api.essentials.MoneyAPI;
+import com.darksoldier1404.dppc.api.inventory.DInventory;
+import com.darksoldier1404.dppc.utils.NBT;
+import com.darksoldier1404.dppc.utils.Quadruple;
+import com.darksoldier1404.dppc.utils.Tuple;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.inventory.ItemStack;
+
+@SuppressWarnings("all")
+public class DPMEvent implements Listener {
+    private final Menu plugin = Menu.getInstance();
+
+    @EventHandler
+    public void onCommand(PlayerCommandPreprocessEvent e) {
+        String cmd = e.getMessage().split(" ")[0].substring(1);
+        plugin.menus.values().forEach(m -> {
+            if (m.getString("Menu.ALIASES") != null && m.getString("Menu.ALIASES").equalsIgnoreCase(cmd)) {
+                e.setCancelled(true);
+                plugin.getServer().dispatchCommand(e.getPlayer(), "dpm open " + m.getString("Menu.NAME"));
+            }
+        });
+    }
+
+    @EventHandler
+    public void onInventoryClose(InventoryCloseEvent e) {
+        if (DPMFunction.currentInv.containsKey(e.getPlayer().getUniqueId())) {
+            DInventory inv = DPMFunction.currentInv.get(e.getPlayer().getUniqueId());
+            if(!inv.isValidHandler(plugin)) return;
+            if (inv.getObj() != null) {
+                DPMFunction.saveItemSetting((Player) e.getPlayer(), ((Tuple<String, String>) inv.getObj()).getA(), inv);
+            }
+            DPMFunction.currentInv.remove(e.getPlayer().getUniqueId());
+        }
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent e) {
+        if (DPMFunction.currentInv.containsKey(e.getWhoClicked().getUniqueId())) {
+            DInventory inv = DPMFunction.currentInv.get(e.getWhoClicked().getUniqueId());
+            if(!inv.isValidHandler(plugin)) return;
+            if (e.getCurrentItem() == null) return;
+            Player p = (Player) e.getWhoClicked();
+            if (inv.getObj() == null) {
+                e.setCancelled(true);
+                if (NBT.hasTagKey(e.getCurrentItem(), "dpm.command")) {
+                    String command = NBT.getStringTag(e.getCurrentItem(), "dpm.command");
+                    if (NBT.hasTagKey(e.getCurrentItem(), "op_cmd")) {
+                        Bukkit.getScheduler().runTaskLater(plugin, () -> {
+                            if(p.isOp()){
+                                p.performCommand(command.replace("<player>", p.getName()));
+                            }else{
+                                p.setOp(true);
+                                p.performCommand(command.replace("<player>", p.getName()));
+                                p.setOp(false);
+                            }
+                        }, 2L);
+                    } else {
+                        Bukkit.getScheduler().runTaskLater(plugin, () -> {
+                            p.performCommand(command.replace("<player>", p.getName()));
+                        }, 2L);
+                    }
+                }
+                if (NBT.hasTagKey(e.getCurrentItem(), "dpm.sound")) {
+                    try {
+                        String sound = NBT.getStringTag(e.getCurrentItem(), "dpm.sound");
+                        float volume = Float.parseFloat(NBT.getStringTag(e.getCurrentItem(), "dpm.sound.volume"));
+                        float pitch = Float.parseFloat(NBT.getStringTag(e.getCurrentItem(), "dpm.sound.pitch"));
+                        p.playSound(p.getLocation(), sound, volume, pitch);
+                    } catch (Exception ex) {
+                        p.sendMessage(plugin.data.getPrefix() + "사운드 설정이 잘못되었습니다.");
+                        p.sendMessage(plugin.data.getPrefix() + "소리 : " + NBT.getStringTag(e.getCurrentItem(), "dpm.sound"));
+                        p.sendMessage(plugin.data.getPrefix() + "볼륨 : " + NBT.getStringTag(e.getCurrentItem(), "dpm.sound_volume"));
+                        p.sendMessage(plugin.data.getPrefix() + "피치 : " + NBT.getStringTag(e.getCurrentItem(), "dpm.sound_pitch"));
+                    }
+                }
+                if (NBT.hasTagKey(e.getCurrentItem(), "dpm.cwc")) {
+                    p.closeInventory();
+                }
+                if (NBT.hasTagKey(e.getCurrentItem(), "dpm.price")) {
+                    String sprice = NBT.getStringTag(e.getCurrentItem(), "dpm.price");
+                    try {
+                        double price = Double.parseDouble(sprice);
+                        MoneyAPI.takeMoney(p, price);
+                    } catch (Exception ex) {
+                        p.sendMessage(plugin.data.getPrefix() + "가격 설정이 잘못되었습니다.");
+                        p.sendMessage(plugin.data.getPrefix() + "가격 : " + NBT.getStringTag(e.getCurrentItem(), "dpm.price"));
+                    }
+                }
+                return;
+            }
+            Tuple<String, String> t = (Tuple<String, String>) inv.getObj();
+            String b = t.getB();
+            if (!b.equalsIgnoreCase("ITEMS")) {
+                e.setCancelled(true);
+                if (b.equalsIgnoreCase("commands")) {
+                    DPMFunction.currentEditItem.put(p.getUniqueId(), Quadruple.of(t.getA(), e.getCurrentItem(), "commands", e.getSlot()));
+                    p.closeInventory();
+                    p.sendMessage(plugin.data.getPrefix() + "설정하실 커맨드를 입력하세요.");
+                }
+                if (b.equalsIgnoreCase("sound")) {
+                    DPMFunction.currentEditItem.put(p.getUniqueId(), Quadruple.of(t.getA(), e.getCurrentItem(), "sound", e.getSlot()));
+                    p.closeInventory();
+                    p.sendMessage(plugin.data.getPrefix() + "설정하실 사운드를 입력하세요. ( EX) ui.button.click 1 1 )");
+                }
+                if (b.equalsIgnoreCase("price")) {
+                    DPMFunction.currentEditItem.put(p.getUniqueId(), Quadruple.of(t.getA(), e.getCurrentItem(), "price", e.getSlot()));
+                    p.closeInventory();
+                    p.sendMessage(plugin.data.getPrefix() + "설정하실 가격을 입력하세요.");
+                }
+                if (b.equalsIgnoreCase("op")) {
+                    if (NBT.hasTagKey(e.getCurrentItem(), "op_cmd")) {
+                        e.setCurrentItem(NBT.removeTag(e.getCurrentItem(), "op_cmd"));
+                        p.sendMessage(plugin.data.getPrefix() + "해당 슬롯을 유저의 권한으로 실행되게 설정하였습니다.");
+                    } else {
+                        e.setCurrentItem(NBT.setStringTag(e.getCurrentItem(), "op_cmd", "true"));
+                        p.sendMessage(plugin.data.getPrefix() + "해당 슬롯을 관리자의 권한으로 실행되게 설정하였습니다.");
+                    }
+                }
+                if (b.equalsIgnoreCase("cwc")) {
+                    if (NBT.hasTagKey(e.getCurrentItem(), "dpm.cwc")) {
+                        e.setCurrentItem(NBT.removeTag(e.getCurrentItem(), "dpm.cwc"));
+                        p.sendMessage(plugin.data.getPrefix() + "해당 슬롯을 클릭시 메뉴를 닫히지 않게 설정하였습니다.");
+                    } else {
+                        e.setCurrentItem(NBT.setStringTag(e.getCurrentItem(), "dpm.cwc", "true"));
+                        p.sendMessage(plugin.data.getPrefix() + "해당 슬롯을 클릭시 메뉴를 닫히게 설정하였습니다.");
+                    }
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent e) {
+        if (DPMFunction.currentEditItem.containsKey(e.getPlayer().getUniqueId())) {
+            e.setCancelled(true);
+            Quadruple<String, ItemStack, String, Integer> t = DPMFunction.currentEditItem.get(e.getPlayer().getUniqueId());
+            if (t.getC().equalsIgnoreCase("commands")) {
+                t.setB(DPMFunction.setCommand(t.getB(), e.getMessage()));
+                Bukkit.getScheduler().runTask(plugin, () -> {
+                    DPMFunction.openCommandSettingGUI(e.getPlayer(), t.getA(), t.getB(), t.getD());
+                    DPMFunction.currentEditItem.remove(e.getPlayer().getUniqueId());
+                });
+            }
+            if (t.getC().equalsIgnoreCase("sound")) {
+                t.setB(DPMFunction.setSound(t.getB(), e.getMessage()));
+                Bukkit.getScheduler().runTask(plugin, () -> {
+                    DPMFunction.openSoundSettingGUI(e.getPlayer(), t.getA(), t.getB(), t.getD());
+                    DPMFunction.currentEditItem.remove(e.getPlayer().getUniqueId());
+                });
+            }
+            if (t.getC().equalsIgnoreCase("price")) {
+                t.setB(DPMFunction.setPrice(t.getB(), e.getMessage()));
+                Bukkit.getScheduler().runTask(plugin, () -> {
+                    DPMFunction.openPriceSettingGUI(e.getPlayer(), t.getA(), t.getB(), t.getD());
+                    DPMFunction.currentEditItem.remove(e.getPlayer().getUniqueId());
+                });
+            }
+        }
+    }
+}
---
+++ v1_16_5/src/main/java/com/darksoldier1404/dpm/commands/DPMCommand.java
@@ -0,0 +1,194 @@
+package com.darksoldier1404.dpm.commands;
+
+import com.darksoldier1404.dpm.Menu;
+import com.darksoldier1404.dpm.functions.DPMFunction;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SuppressWarnings("all")
+public class DPMCommand implements CommandExecutor, TabCompleter {
+    private final Menu plugin = Menu.getInstance();
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(plugin.data.getPrefix() + "플레이어만 사용 가능한 명령어 입니다.");
+            return false;
+        }
+        Player p = (Player) sender;
+        if (args.length == 0) {
+            if (p.hasPermission("dpm.use")) {
+                p.sendMessage(plugin.data.getPrefix() + "/dpm open <name> - 메뉴를 오픈합니다.");
+            }
+            if (p.hasPermission("dpm.admin")) {
+                p.sendMessage(plugin.data.getPrefix() + "/dpm create <name> <row> - 메뉴를 생성합니다.");
+                p.sendMessage(plugin.data.getPrefix() + "/dpm delete <name> - 메뉴를 삭제합니다.");
+                p.sendMessage(plugin.data.getPrefix() + "/dpm title <name> <title> - 메뉴의 타이틀을 설정합니다.");
+                p.sendMessage(plugin.data.getPrefix() + "/dpm row <name> <row> - 메뉴의 행을 설정합니다.");
+                p.sendMessage(plugin.data.getPrefix() + "/dpm items <name> - 메뉴의 아이템을 설정합니다.");
+                p.sendMessage(plugin.data.getPrefix() + "/dpm cmds <name> - 메뉴의 클릭시 커맨드를 설정합니다.");
+                p.sendMessage(plugin.data.getPrefix() + "/dpm op <name> - 메뉴의 커맨드를 OP권한/일반 권한으로 설정합니다.");
+                p.sendMessage(plugin.data.getPrefix() + "/dpm cwc <name> - 메뉴의 아이템 클릭시 메뉴를 닫히게 할지 설정합니다. (CloseWhenClick)");
+                p.sendMessage(plugin.data.getPrefix() + "/dpm sound <name> - 메뉴의 클릭시 사운드를 설정합니다.");
+                p.sendMessage(plugin.data.getPrefix() + "/dpm price <name> - 메뉴의 클릭시 이용 요금을 설정합니다. (에센셜 필요)");
+                p.sendMessage(plugin.data.getPrefix() + "/dpm aliases <name> <cmd> - 메뉴의 단축 명령어를 설정합니다.");
+                p.sendMessage(plugin.data.getPrefix() + "/dpm list - 모든 메뉴를 보여줍니다.");
+                p.sendMessage(plugin.data.getPrefix() + "/dpm reload - 메뉴를 리로드합니다.");
+            }
+            return false;
+        }
+        if (!p.hasPermission("dpm.use")) {
+            p.sendMessage(plugin.data.getPrefix() + "권한이 없습니다.");
+            return false;
+        }
+        if (args[0].equalsIgnoreCase("open")) {
+            if (args.length == 1) {
+                p.sendMessage(plugin.data.getPrefix() + "오픈할 메뉴의 이름을 입력해주세요.");
+                return false;
+            }
+            DPMFunction.openMenu(p, args[1]);
+            return false;
+        }
+        if (!p.hasPermission("dpm.admin")) {
+            p.sendMessage(plugin.data.getPrefix() + "권한이 없습니다.");
+            return false;
+        }
+        if (args[0].equalsIgnoreCase("create")) {
+            if (args.length == 1) {
+                p.sendMessage(plugin.data.getPrefix() + "생성할 메뉴의 이름을 입력해주세요.");
+                return false;
+            }
+            if (args.length == 2) {
+                p.sendMessage(plugin.data.getPrefix() + "생성할 메뉴의 행을 입력해주세요.");
+                return false;
+            }
+            DPMFunction.createMenu(p, args[1], args[2]);
+            return false;
+        }
+        if (args[0].equalsIgnoreCase("delete")) {
+            if (args.length == 1) {
+                p.sendMessage(plugin.data.getPrefix() + "삭제할 메뉴의 이름을 입력해주세요.");
+                return false;
+            }
+            DPMFunction.deleteMenu(p, args[1]);
+            return false;
+        }
+        if (args[0].equalsIgnoreCase("title")) {
+            if (args.length == 1) {
+                p.sendMessage(plugin.data.getPrefix() + "설정할 메뉴의 이름을 입력해주세요.");
+                return false;
+            }
+            if (args.length == 2) {
+                p.sendMessage(plugin.data.getPrefix() + "설정할 메뉴의 타이틀을 입력해주세요.");
+                return false;
+            }
+            DPMFunction.setTitle(p, args[1], args);
+            return false;
+        }
+        if (args[0].equalsIgnoreCase("row")) {
+            if (args.length == 1) {
+                p.sendMessage(plugin.data.getPrefix() + "설정할 메뉴의 이름을 입력해주세요.");
+                return false;
+            }
+            if (args.length == 2) {
+                p.sendMessage(plugin.data.getPrefix() + "설정할 메뉴의 행을 입력해주세요.");
+                return false;
+            }
+            DPMFunction.setRow(p, args[1], args[2]);
+            return false;
+        }
+        if (args[0].equalsIgnoreCase("items")) {
+            if (args.length == 1) {
+                p.sendMessage(plugin.data.getPrefix() + "설정할 메뉴의 이름을 입력해주세요.");
+                return false;
+            }
+            DPMFunction.openItemSettingGUI(p, args[1]);
+            return false;
+        }
+        if (args[0].equalsIgnoreCase("cmds")) {
+            if (args.length == 1) {
+                p.sendMessage(plugin.data.getPrefix() + "설정할 메뉴의 이름을 입력해주세요.");
+                return false;
+            }
+            DPMFunction.openCommandSettingGUI(p, args[1]);
+            return false;
+        }
+        if (args[0].equalsIgnoreCase("op")) {
+            if (args.length == 1) {
+                p.sendMessage(plugin.data.getPrefix() + "설정할 메뉴의 이름을 입력해주세요.");
+                return false;
+            }
+            DPMFunction.openOPSettingGUI(p, args[1]);
+            return false;
+        }
+        if (args[0].equalsIgnoreCase("sound")) {
+            if (args.length == 1) {
+                p.sendMessage(plugin.data.getPrefix() + "설정할 메뉴의 이름을 입력해주세요.");
+                return false;
+            }
+            DPMFunction.openSoundSettingGUI(p, args[1]);
+            return false;
+        }
+        if (args[0].equalsIgnoreCase("cwc")) {
+            if (args.length == 1) {
+                p.sendMessage(plugin.data.getPrefix() + "설정할 메뉴의 이름을 입력해주세요.");
+                return false;
+            }
+            DPMFunction.openCWCSettingGUI(p, args[1]);
+            return false;
+        }
+        if (args[0].equalsIgnoreCase("price")) {
+            if (args.length == 1) {
+                p.sendMessage(plugin.data.getPrefix() + "설정할 메뉴의 이름을 입력해주세요.");
+                return false;
+            }
+            DPMFunction.openPriceSettingGUI(p, args[1]);
+            return false;
+        }
+        if (args[0].equalsIgnoreCase("aliases")) {
+            if (args.length == 1) {
+                p.sendMessage(plugin.data.getPrefix() + "설정할 메뉴의 이름을 입력해주세요.");
+                return false;
+            }
+            if (args.length == 2) {
+                p.sendMessage(plugin.data.getPrefix() + "메뉴의 단축 명령어를 입력해주세요.");
+                return false;
+            }
+            DPMFunction.setAliases(p, args[1], args[2]);
+            return false;
+        }
+        if (args[0].equalsIgnoreCase("list")) {
+            p.sendMessage(plugin.data.getPrefix() + "<<< 메뉴 목록 >>>");
+            plugin.menus.keySet().forEach(key -> {
+                p.sendMessage(plugin.data.getPrefix() + " - " + key);
+            });
+            return false;
+        }
+        if (args[0].equalsIgnoreCase("reload")) {
+            plugin.data.reload();
+            p.sendMessage(plugin.data.getPrefix() + "설정을 다시 불러왔습니다.");
+            return false;
+        }
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+        if (args.length == 1) {
+            if (sender.hasPermission("dpm.admin")) {
+                return Arrays.asList("open", "create", "delete", "title", "row", "items", "cmds", "op", "sound", "cwc", "price", "reload", "aliases");
+            }
+            return Arrays.asList("open");
+        }
+        return null;
+    }
+}
